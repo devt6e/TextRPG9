3c16c99e13f7ab9a3e94f73f0f683efe06c35698
yun3990 <ysk3990@gmail.com>
2026-07-30 19:50:05 +0900

    Battle기본

--- include/core/BattleManager.h
+++ include/core/BattleManager.h
@@ -1,15 +1,28 @@
 #pragma once
+#include <iostream>
+#include <string>
 
+//임시 플레이어 몬스터 보고 가져올 예정임
 class Player;
 class Monster;
 
-class BattleResult
+enum class BattleResult
 {
-    Victory,
-    Defeat,
-    Escaped
+    Victory,//승리
+    Defeat,//패배
+    Escaped//런
 };
+
 class BattleManager
 {
+public:
+    //메인에서? 전투 할때 사용
+    BattleResult StartBattle(Player& player, Monster& monster);
 
+private://임시
+    void PlayerAttack(Player& player, Monster& monster);//공격
+    void MonsterAttack(Monster& monster, Player& player);//몬스터 공격
+    bool TryEscape(Player& player);//런 시도
+    bool UseItem(Player& player);// 템사용
 };
+
--- include/core/DungeonManager.h
+++ include/core/DungeonManager.h
@@ -1,14 +1,35 @@
 #pragma once
+#include <iostream>
+#include <string>
 
 #include "BattleManager.h"
 
+//임시
+class Player;
+class Monster;
 
-class RoomType
+enum class RoomType
 {
-
+    Monster,
+    NPC,
+    Boss
 };
 
 class DungeonManager
 {
+private:
+    BattleManager battleManager;
+    int currentRoom;
 
+public:
+    DungeonManager();
+
+    void StartDungeon(Player& player);
+    void MoveRoom(Player& player);
+
+private:
+    //임시
+    RoomType DecideRoomType();
+    void HandleRoom(Player& player, RoomType roomType);
+    void HandleBattleResult(BattleResult result);
 };
