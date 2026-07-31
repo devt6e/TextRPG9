af27015ed616abc525f2a71c879e8277a473de96
kwbang03 <kwbang03@gmail.com>
2026-07-31 10:58:42 +0900

    UI Update 22

--- include/core/UI/UI2.h
+++ include/core/UI/UI2.h
@@ -13,30 +13,32 @@ private:
     int MenuChoice;
     int PlayerLocation = 0;
     int CurrentFloor = 1;
-    std::vector<std::string> menu = { "던전","인벤토리" };
+    std::vector<std::string> MainMenu = { "던전","인벤토리","제작소","게임종료"};
+    std::vector<std::string> CraftMenu = { "레시피 전체 조회","레시피 검색","제작" };
 
 public:
-    void MainTitle()
-    void MiniTitle()
-    void ZepBuilding()
-    void NBCTown()
-    void Intro()
-    void Dungeon()
-    void PlayerMenu()
-    void Maps1()
-    void Maps1_1()
-    void Maps1_2()
-    void Maps1_3()
-    void Maps1_4()
-    void Maps1_5()
-    void Maps2()
-    void Maps2_1()
-    void Maps2_2()
-    void Maps2_3()
-    void Maps2_4()
-    void Maps2_5()
-    void NPC_M()
-    void NPC_K()
+    void MainTitle();
+    void MiniTitle();
+    void ZepBuilding();
+    void NBCTown();
+    void Intro();
+    void Status();
+    void Dungeon();
+    void PlayerMenu();
+    void Maps1();
+    void Maps1_1();
+    void Maps1_2();
+    void Maps1_3();
+    void Maps1_4();
+    void Maps1_5();
+    void Maps2();
+    void Maps2_1();
+    void Maps2_2();
+    void Maps2_3();
+    void Maps2_4();
+    void Maps2_5();
+    void NPC_M();
+    void NPC_K();
     
     
 
