24767601850d4f8a73efb223e09250bab16c2373
TaeHyeong <devt6e@gmail.com>
2026-08-04 04:20:51 +0900

    feat: 상점 구조 추가, 인벤토리 생각만

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,9 +1,10 @@
-//
-//﻿#include <iostream>
-//
-//#include "core/GamaManager.h"
-//
-//int main()
-//{
-//	GameManager().Run();
-//}
+﻿#include <iostream>
+#include "core/GamaManager.h"
+
+int main()
+{
+	SetConsoleOutputCP(CP_UTF8);
+	SetConsoleCP(CP_UTF8);
+
+	GameManager().Run();
+}
