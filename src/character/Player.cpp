bcb27e12049f96725f909050e3b363696a339469
RED-0107 <kimbobin1004@gmail.com>
2026-08-03 16:22:34 +0900

    player.fix 수정

--- src/character/Player.cpp
+++ src/character/Player.cpp
@@ -1,10 +1,6 @@
 #include "character/Player.h"
 #include <iostream>
 
-// 생성자: 이름과 기본 스탯(레벨 1, HP 200, 공격력 30, 경험치 0/100) 초기화
-Player::Player(std::string Name) : Name(Name) {
-}
-
 // 최대 체력을 넘거나 0 미만으로 떨어지지 않게 하는 함수
 void Player::SetHp(int Value) {
     Hp = Value;
