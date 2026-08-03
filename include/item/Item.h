3222a2f6cc649ca319ed4ffd68a7da04471259ae
Hj777-1 <halfmoon0322@gmail.com>
2026-08-03 14:46:43 +0900

    update Item

--- include/item/Item.h
+++ include/item/Item.h
@@ -6,25 +6,29 @@
 
 using namespace std;
 
+/// 임시 플레이어
+/*
 struct Player {
 	int Hp{ 50 };
 	int MaxHp{ 200 };
 	int Power{ 50 };
 	int TempAttackBuff{ 0 };
 
-	void setHp(int newHp) { Hp = newHp; }
-	void setMaxHp(int MH) { MaxHp = MH; }
-	void setPower(int p) { Power = p; }
+	void SetHp(int newHp) { Hp = newHp; }
+	void SetMaxHp(int MH) { MaxHp = MH; }
+	void SetPower(int p) { Power = p; }
 
-	int getHp() const { return Hp; }
-	int getMaxHp() const { return MaxHp; }
+	int GetHp() const { return Hp; }
+	int GetMaxHp() const { return MaxHp; }
 
-	int getTempAttackBuff() const { return TempAttackBuff; }
-	int getPower() const { return Power; }
-	int getTotalPower() const { return Power + TempAttackBuff; }
+	int GetTempAttackBuff() const { return TempAttackBuff; }
+	int GetPower() const { return Power; }
+	int GetTotalPower() const { return Power + TempAttackBuff; }
 
 	void ResetBuff() { TempAttackBuff = 0; }
-};
+}; */
+
+
 
 class Item {
 public:
